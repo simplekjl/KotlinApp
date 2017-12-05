415f559874891b7096227759d06787f035fd2a2d
Jose Luis <jlcs@Joses-MacBook-Pro.local>
2017-12-04 16:03:15 -0800

    KotlinFS | Operations Sum, division , etc

---
+++ src/Operations.kt
@@ -0,0 +1,17 @@
+/**
+ * Math Calculator
+ */
+fun main(args: Array<String>){
+
+    println("Math Calculator")
+    print("Enter number 1 : ")
+    var n1 : Float = readLine()!!.toFloat()
+    print("Enter number 2: ")
+    var n2 : Float = readLine()!!.toFloat()
+    var sum : Float?
+    sum = n1+ n2
+    println ("The result is: $sum")
+    //var n
+    //var n
+
+}
